e153e271b35068b17f5c99c32b6f2e0a192b80e3
Divyansh1Rai6 <divyansh16rai@gmail.com>
2026-08-20 05:45:17 +0530

    Removed Admin Portal, Store ownner dashboard, Website builder from current UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,7 @@ import { Partnerships } from "../components/Partnerships";
 import { Testimonials } from "../components/Testimonials";
 import { About } from "../components/About";
 import { Footer } from "../components/Footer";
-import { StoreOwnerDashboardShowcase } from "@/components/StoreOwnerDashboardShowcase";
-import { WebsiteBuilder } from "@/components/WebsiteBuilder";
+
 
 export default function HomePage() {
   return (
@@ -24,8 +23,6 @@ export default function HomePage() {
       <Pricing />
       <Partnerships />
       <Testimonials />
-      <StoreOwnerDashboardShowcase />
-      <WebsiteBuilder />
       <About />
       <Footer />
     </div>
